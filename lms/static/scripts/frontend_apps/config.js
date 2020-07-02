366394053e72dc331ce4f3ce5ce3b53b75a4b87f
Robert Knight <robertknight@gmail.com>
2020-07-02 19:02:21 +0100

    Suppress typechecking errors related to server config.
    Filling this out will be done in a separate step.

--- lms/static/scripts/frontend_apps/config.js
+++ lms/static/scripts/frontend_apps/config.js
@@ -4,4 +4,13 @@ import { createContext } from 'preact';
  * Configuration object for the file picker application, read from a JSON
  * script tag injected into the page by the backend.
  */
-export const Config = createContext({ api: {} });
+
+// This is the place to define the complete set of config properties that the
+// backend might set.
+//
+// For now we just use `any` and don't check this at build time.
+
+/** @type {any} */
+const defaultConfig = { api: {} };
+
+export const Config = createContext(defaultConfig);
